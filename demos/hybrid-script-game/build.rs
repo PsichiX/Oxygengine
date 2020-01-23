360f598b8b01d2b6c6a7a9ca06bd5841a404f576
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2020-01-23 05:36:39 +0100

    disable hybrid scripting demo assets build pipeline

--- demos/hybrid-script-game/build.rs
+++ demos/hybrid-script-game/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), std::io::Error> {
-    oxygengine_build_tools::pipeline::Pipeline::from_file("pipeline.json", true)?.execute()?;
+    // oxygengine_build_tools::pipeline::Pipeline::from_file("pipeline.json", true)?.execute()?;
     Ok(())
 }
