f6c5d024271e80cff88c532bd2adc23a87ae2270
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2020-07-01 17:39:18 +0200

    snapshot

--- oxygengine-procedural/src/lib.rs
+++ oxygengine-procedural/src/lib.rs
@@ -4,10 +4,11 @@ extern crate rayon;
 pub mod wave_function_collapse;
 pub mod world_2d;
 pub mod world_2d_climate_simulation;
+pub use oxygengine_utils::{grid_2d::*, noise_map_generator::*, Scalar};
 
 pub mod prelude {
     pub use crate::wave_function_collapse::*;
     pub use crate::world_2d::*;
     pub use crate::world_2d_climate_simulation::*;
-    pub use oxygengine_utils::Scalar;
+    pub use oxygengine_utils::{grid_2d::*, noise_map_generator::*, Scalar};
 }
